9e63a8bd49a9c97c5ec730b2d494c53031045eca
Pierre Planex <pierre.planex@gmail.com>
2026-03-03 17:42:45 +0100

    definition controller programs

---
+++ app/controllers/programs_controller.rb
@@ -0,0 +1,32 @@
+class ProgramsController < ApplicationController
+  def index
+    @programs = Program.all
+  end
+
+  def show
+    @program = Program.find(params[:id])
+  end
+
+  def new
+    @program = Program.new
+  end
+
+  def create
+    @program = Program.new(program_params)
+    @program.user = current_user
+
+    if @program.save
+      @chat = Chat.create(program: @program)
+      #appelle à l'ia 
+      redirect_to @program, notice: "program was successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def program_params
+    params.require(:program).permit(:goal, :level, :medical_conditions, :number_of_week, :sport, :tools)
+  end
+end
