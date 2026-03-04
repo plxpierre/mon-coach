17be6f4128818db4b83e9732dc9af31571c53b24
Pierre Planex <pierre.planex@gmail.com>
2026-03-04 12:27:42 +0100

    set views: show / index / new / create

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,8 +1,10 @@
 class ProgramsController < ApplicationController
-  before_action :authenticate_user!
-
   def index
-    @programs = current_user.programs
+    @programs = Program.all
+  end
+
+  def show
+    @program = Program.find(params[:id])
   end
 
   def new
@@ -10,6 +12,21 @@ class ProgramsController < ApplicationController
   end
 
   def create
-    raise
+    @program = Program.new(program_params)
+    @program.user = current_user
+
+    if @program.save
+      @chat = Chat.create(program: @program)
+      #appel à l'ia
+      redirect_to @program, notice: "program was successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def program_params
+    params.require(:program).permit(:goal, :level, :medical_conditions, :number_of_week, :sport, :tools)
   end
 end
