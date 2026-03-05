285836e9a0820fba36f2111a9f78b546ded63ea0
Nicolas Martinez <5d9r94bfhg@privaterelay.appleid.com>
2026-03-05 10:33:10 +0100

    Front end

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,8 +1,20 @@
+# app/controllers/programs_controller.rb
+require "ostruct"
+
 class ProgramsController < ApplicationController
   before_action :authenticate_user!
 
   def index
     @programs = current_user.programs
+
+    # Temporary fallback: show demo cards until create is implemented
+    if @programs.empty?
+      @programs = [
+        OpenStruct.new(id: 1, title: "Hybrid Strength"),
+        OpenStruct.new(id: 2, title: "Endurance Builder"),
+        OpenStruct.new(id: 3, title: "Bodyweight Elite")
+      ]
+    end
   end
 
   def new
@@ -10,6 +22,5 @@ class ProgramsController < ApplicationController
   end
 
   def create
-    raise
   end
 end
