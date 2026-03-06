4cbd2f34d04155a85fbc93c3bec89b2b5678799e
Aude Houbron <aude.houbron@gmail.com>
2026-03-06 12:23:05 +0100

    ajustement du prompt et de l'itération dans la show

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -19,20 +19,17 @@ Each week must contain exactly the number of sessions adapted to the goal.
 
 Each session must be a string in Markdown format with:
 
-A title formatted as: '### Day: Session Name'
-
 A short introductory paragraph describing the session.
 
-A bullet list of the exercises and sequence.
+A list of the exercises and sequence.
 
 A final sentence explaining the purpose of the session.
 
 Adapt intensity, progression, and exercises to the provided level and constraints.
 
-Must be a valid JSON
+Must be a valid parsable JSON
 week_program must be an array
-Return the exercises strictly as a Markdown bullet list using '*' with one exercise per line.
-Do not place multiple exercises on the same line.
+Return the exercises strictly as a list.
 Expected structure:
 Start your response with: {
 
@@ -42,19 +39,53 @@ Start your response with: {
 {
 'week_number': 1,
 'week_program': [
-'### **Monday: Session Name**\n\n**Description**...\n\n**Exercises:**\n*...\n\nFinal sentence  ...',
-'### **Wednesday: Session Name**\n\n**Description**...\n\n**Exercises:**\n*...\n\nFinal sentence  ...',
-'### **Friday: Session Name**\n\n**Description**...\n\n**Exercises:**\n*...\n\nFinal sentence  ...'
-]
-},
+  {
+    'day': 'Monday',
+    'session_name': value,
+    'description': value,
+    'exercises': [values],
+    'final_sentence': value
+  },
+    {
+    'day': 'Wednesday',
+    'session_name': value,
+    'description': value,
+    'exercises': [values],
+    'final_sentence': value
+  },
+    {
+    'day': 'Friday',
+    'session_name': value,
+    'description': value,
+    'exercises': [values],
+    'final_sentence': value
+  }
+}
 {
 'week_number': 2,
 'week_program': [
-'### **Monday: Session Name**\n\n**Description**...\n\n**Exercises:**\n*...\n\nFinal sentence  ...',
-'### **Wednesday: Session Name**\n\n**Description**...\n\n**Exercises:**\n*...\n\nFinal sentence  ...',
-'### Friday: Session Name\n\n**Description**...\n\n**Exercises:**\n*...\n\nFinal sentence  ...'
-]
-}
+  {
+    'day': 'Monday',
+    'session_name': value,
+    'description': value,
+    'exercises': [values],
+    'final_sentence': value
+  },
+    {
+    'day': 'Wednesday',
+    'session_name': value,
+    'description': value,
+    'exercises': [values],
+    'final_sentence': value
+  },
+    {
+    'day': 'Friday',
+    'session_name': value,
+    'description': value,
+    'exercises': [values],
+    'final_sentence': value
+  }
+} 
 ]
 }
 "
